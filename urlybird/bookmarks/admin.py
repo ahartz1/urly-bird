044720f7dfabdcde2896b5968807ad84dcf67daa
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-10-15 15:03:34 -0400

    models registered

--- urlybird/bookmarks/admin.py
+++ urlybird/bookmarks/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Worm, Click
+
+class WormAdmin(admin.ModelAdmin):
+    list_display = ['flink', 'slink', 'user', 'timestamp', 'wtitle', 'winfo', 'numclicks']
+
+
+class ClickAdmin(admin.ModelAdmin):
+    list_display = ['user', 'worm', 'timestamps']
 
 # Register your models here.
+
+admin.site.register(Worm, WormAdmin)
+admin.site.register(Click, ClickAdmin)
