cf255c1ca47089bd616660bb9e2037252cabf9f2
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-10-15 14:03:04 -0400

    Worm and Click models created

--- urlybird/bookmarks/models.py
+++ urlybird/bookmarks/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Worm(models.Model):
+    flink = models.TextField(max_length=2000)  # full url
+    slink = models.CharField(max_length=200)  # short url
+    user = models.ForeignKey(User)
+    timestamp = models.DateTimeField()
+    winfo = models.CharField(max_length=255)
+
+
+class Click(models.Models):
+    user = models.ForeignKey(User, null=True, blank=True)
+    worm = models.ForeignKey(Worm)
+    timestamp = models.DateTimeField()
