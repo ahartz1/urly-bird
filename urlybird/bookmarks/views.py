15c9aaf6b90e6bde14a576d5d5e77cf811e6e607
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-10-15 19:26:48 -0400

    3 basic generic views

--- urlybird/bookmarks/views.py
+++ urlybird/bookmarks/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Worm, Click
 
 # Create your views here.
+
+
+class BirdIndexView(generic.ListView):
+    template_name = 'bookmarks/all_worms.html'
+    context_object_name = 'all_worms'
+    paginate_by = 25
+
+    def get_queryset(self):
+        return Worm.objects.all().order_by('-timestamp')
+
+
+class WormIndexView(generic.ListView):
+    template_name = 'bookmarks/bird_detail.html'
+    context_object_name = 'all_worms'
+    paginate_by = 25
+
+    def get_queryset(self):
+        return self.user.rating_set.all().order_by('-timestamp')
+
+
+class WormDetailView(generic.DetailView):
+    model = Worm
+    template = 'bookmarks/worm_detail.html'
