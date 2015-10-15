af6263484162ab3cded3d61f7f90e51e8ea0ac38
Adam Hartz <ahartz1@gmail.com>
2015-10-15 19:58:29 -0400

    Further ideas on app urls

--- urlybird/bookmarks/urls.py
+++ urlybird/bookmarks/urls.py
@@ -4,7 +4,7 @@ from . import views as bviews
 
 
 urlpatterns = [
-    # url(r'^worms/(?P<pk>\w+)$', ),
-    # url(r'^birds/$', ),
-    # url(r'^clicks/$', ),
+    # url(r'^worms/(?P<pk>\w+)$', bviews.worm_detail, name='worm_detail'),
+    # url(r'^birds/(?P<pk>\w+)$', bviews.bird_detail, name='bird_detail' ),
+    # url(r'^clicks/$', bviews.click_list, name='top_click'),
 ]
