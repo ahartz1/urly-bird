1d731bea6da628105fde4f6d89db029fc43ef790
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-10-16 13:50:10 -0400

    draft 1 views done

--- urlybird/bookmarks/views.py
+++ urlybird/bookmarks/views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect, render, get_object_or_404
-from urlybird.forms import UserForm
+from urlybird.forms import UserForm#, WormForm
 from django.views import generic
 from .models import Worm
 from django.contrib.auth.models import User
@@ -18,6 +18,7 @@ class WormListView(generic.ListView):
     paginate_by = 25
 
     def get_queryset(self):
+        self.form = WormForm()
         return Worm.objects.all().order_by('-timestamp')
 
 
@@ -27,7 +28,9 @@ class BirdListView(generic.ListView):
     paginate_by = 25
 
     def get_queryset(self):
+        self.form = WormForm()
         # user = User.objects.get(pk=user.pk)
+
         user = get_object_or_404(User, pk=self.kwargs['pk'])
 
         return user.worm_set.all().order_by('-timestamp')
